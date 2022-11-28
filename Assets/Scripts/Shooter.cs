c8222453c52af5ee74c7a99160e4cae3329dac0b
William Convertino <williamconvertino@gmail.com>
2022-11-28 23:41:45 +0100

    Added grabbable gun

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    public GameObject rayOrigin;
+    public GameObject destinationPrefab;
+
+    
+    
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        
+    }
+}
