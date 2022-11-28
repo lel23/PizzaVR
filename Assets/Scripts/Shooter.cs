351b50dfae33c1682f938310895fbfdcbe83ad9b
William Convertino <williamconvertino@gmail.com>
2022-11-29 00:41:16 +0100

    Updated to two pivot points

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,7 +7,8 @@ public class Shooter : MonoBehaviour
 
     public GameObject rayOrigin;
     public GameObject destinationPrefab;
-
+    public GameObject initialPrefab;
+    
     public void FireShooter()
     {
         RaycastHit hit;
@@ -17,6 +18,50 @@ public class Shooter : MonoBehaviour
             return;
         }
 
-        Instantiate(destinationPrefab, hit.point, Quaternion.identity);
+        ReleaseDestination();
+        destinationLocked = true;
+        destinationCoordinates = hit.point;
+        destinationObject = Instantiate(destinationPrefab, destinationCoordinates, Quaternion.identity);
+        
+        
+        ReleaseInitial();
+        initialLocked = true;
+        initialCoordinates = transform.position;
+        initialObject = Instantiate(initialPrefab, initialCoordinates, Quaternion.identity);
     }
+
+    public void ReleaseShooter()
+    {
+        ReleaseDestination();
+        ReleaseInitial();
+    }
+    
+    private Vector3 destinationCoordinates;
+    private GameObject destinationObject;
+    private bool destinationLocked = false;
+
+    private void ReleaseDestination()
+    {
+        if (!destinationLocked)
+        {
+            return;
+        }
+        Destroy(destinationObject);
+        destinationLocked = false;
+    }
+    
+    private Vector3 initialCoordinates;
+    private GameObject initialObject;
+    private bool initialLocked = false;
+    
+    private void ReleaseInitial()
+    {
+        if (!initialLocked)
+        {
+            return;
+        }
+        Destroy(initialObject);
+        initialLocked = false;
+    }
+    
 }
