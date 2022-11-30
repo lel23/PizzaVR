e515e5ec1bca7c35a9095db4d2af656c2dd6aa4a
justinlong5000 <justinlong5000@gmail.com>
2022-11-30 02:27:30 +0100

    created pizza box and pizza recipient prefabs

---
+++ Assets/Scripts/Game Logic/Pizza.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pizza : MonoBehaviour
+{
+    
+    private Transform t;
+    private Rigidbody rb;
+    private BoxCollider bc;
+    public float floatDuration = .5f;
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+        bc = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collided");
+        // if the pizza recipient collides with the pizza box (i.e. the player hits their target)
+        if (collision.collider.CompareTag("PizzaRecipient"))
+        {
+            Debug.Log("collided with pizza guy");
+            // make the pizza box float front of the recipient (so it looks like they're holding it)
+            rb.useGravity = false;
+            bc.enabled = false;
+            Vector3 recipientPosition = collision.collider.gameObject.transform.position;
+            Vector3 targetPosition = new Vector3(recipientPosition.x, recipientPosition.y, recipientPosition.z - 2);
+            StartCoroutine(floatTowards(targetPosition, floatDuration));
+
+            Quaternion targetRotation = new Quaternion(0, 0, 0, 1);
+            StartCoroutine(rotateTowards(targetRotation, floatDuration));
+
+            Debug.Log("started coroutines");
+
+        }
+    }
+
+    IEnumerator floatTowards(Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+    }
+    IEnumerator rotateTowards(Quaternion targetRotation, float duration)
+    {
+        float time = 0;
+        Quaternion startRotation = transform.rotation;
+        while (time < duration)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = targetRotation;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+}
