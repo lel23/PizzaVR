693b0534adbba6908f107eefdd6b30917e7a10ed
justinlong5000 <justinlong5000@gmail.com>
2022-12-05 17:30:52 +0100

    updating scripts

--- Assets/Scripts/Util/GameManager.cs
+++ Assets/Scripts/Util/GameManager.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Specialized;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private List<GameObject> pizzaRecipients;
+    private OrderedDictionary pizzasDelivered;
     void Start()
     {
-        
+
+        // initialize an ordered dictionary that stores all data about pizzas delivered
+        pizzasDelivered = new OrderedDictionary();
+        foreach (GameObject pizzaRecipient in pizzaRecipients)
+        {
+            pizzasDelivered.Add(pizzaRecipient, false);
+        }
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public Vector3 getNextPizzaRecipientPosition()
     {
         
     }
