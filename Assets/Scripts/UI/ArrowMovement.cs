c3428e29df1aa56e28dcb1ee1fb46f50696a129c
justinlong5000 <justinlong5000@gmail.com>
2022-12-08 14:48:37 +0100

    arrow progress?

--- Assets/Scripts/UI/ArrowMovement.cs
+++ Assets/Scripts/UI/ArrowMovement.cs
@@ -9,9 +9,21 @@ public class ArrowMovement : MonoBehaviour
     private GameObject VRPlayer;
     [SerializeField]
     private GameObject desktopPlayer;
-    private Transform playerCameraTransform;
     public Vector3 nextPizzaRecipientPosition;
     public Vector3 dir;
+
+
+    private GameObject yRotationLoc;
+
+    [SerializeField]
+    private GameObject xRotationLocForDesktop;
+    private GameObject xRotationLoc;
+    private GameObject zRotationLoc;
+
+    public Quaternion currentRotationQuaternion;
+
+    public GameObject crosshair;
+
     void Start()
     {
         if (Singleton.Instance.GameManager.isVR)
@@ -21,15 +33,28 @@ public class ArrowMovement : MonoBehaviour
         else
         {
             player = desktopPlayer;
+            yRotationLoc = desktopPlayer;
+            xRotationLoc = xRotationLocForDesktop;
         }
         nextPizzaRecipientPosition = Singleton.Instance.GameManager.getNextPizzaRecipientPosition();
-        playerCameraTransform = player.transform;
     }
 
     void Update()
     {
-        dir = Vector3.Normalize(nextPizzaRecipientPosition - player.transform.position);
-        transform.LookAt(transform.TransformDirection(dir));
+        dir = Vector3.Normalize(nextPizzaRecipientPosition - crosshair.transform.position);
+        Debug.Log(crosshair.transform.position);
+        if (player == desktopPlayer)
+        {
+            //currentRotationQuaternion = new Quaternion(xRotationLoc.transform.rotation.eulerAngles.x, yRotationLoc.transform.rotation.eulerAngles.y, 0, 1);
+
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+        else if (player == VRPlayer)
+        {
+            currentRotationQuaternion = new Quaternion(xRotationLoc.transform.rotation.eulerAngles.x, yRotationLoc.transform.rotation.eulerAngles.y, zRotationLoc.transform.rotation.eulerAngles.z, 1);
+        }
+        
+        //transform.rotation = Quaternion.LookRotation(dir) * currentRotationQuaternion;
     }
 
     void updatePizzaRecipient()
