12d8d828e6e1333aef44ef25a3396632998ff5d8
William Convertino <williamconvertino@gmail.com>
2022-12-12 23:26:02 +0100

    Added speed grapple

--- Assets/Scripts/Grapple/GrappleController.cs
+++ Assets/Scripts/Grapple/GrappleController.cs
@@ -13,13 +13,19 @@ public class GrappleController : MonoBehaviour
     [Header("Grapple Settings")] 
     public float grappleSpeed = 2.0f;
 
+    private float _currentGrappleSpeed;
+
     [Header("Player Controller")]
     public PlayerController playerController;
 
     private bool _grappleHoldsLock = false;
 
     public float minGrappleSize = 0.2f;
-    
+
+    private void Start()
+    {
+        _currentGrappleSpeed = grappleSpeed;
+    }
     private void Update()
     {
         if (!_grappleHoldsLock && (rightShooter.shooterState == Shooter.ShooterState.Locked || leftShooter.shooterState == Shooter.ShooterState.Locked))
@@ -29,6 +35,7 @@ public class GrappleController : MonoBehaviour
 
         if (rightShooter.shooterState != Shooter.ShooterState.Locked && leftShooter.shooterState != Shooter.ShooterState.Locked)
         {
+            UnspeedGrapple();
             if (_grappleHoldsLock)
             {
                 PlayerLocks.Instance.UnlockPlayer();
@@ -45,16 +52,26 @@ public class GrappleController : MonoBehaviour
             (Vector3.Distance(rightShooter.DestinationMarker.transform.position, rightShooter.transform.position) > minGrappleSize) || rightShooter.JoystickValue.y < 0)
         {
             var grappleDirection = (rightShooter.DestinationMarker.transform.position - rightShooter.rayOrigin.transform.position).normalized;
-            grappleForceRight = grappleDirection * rightShooter.JoystickValue.y * grappleSpeed;
+            grappleForceRight = grappleDirection * rightShooter.JoystickValue.y * _currentGrappleSpeed;
         }
         if (leftShooter.shooterState == Shooter.ShooterState.Locked && 
             (Vector3.Distance(leftShooter.DestinationMarker.transform.position, leftShooter.transform.position) > minGrappleSize) || leftShooter.JoystickValue.y < 0)
         {
             var grappleDirection = (leftShooter.DestinationMarker.transform.position - leftShooter.rayOrigin.transform.position).normalized;
-            grappleForceLeft = grappleDirection * leftShooter.JoystickValue.y * grappleSpeed;
+            grappleForceLeft = grappleDirection * leftShooter.JoystickValue.y * _currentGrappleSpeed;
         }
         
         playerController.lockedVelocity += grappleForceLeft + grappleForceRight;
+    }
+
+    public void SpeedGrapple()
+    {
+        _currentGrappleSpeed = 2 * grappleSpeed;
+    }
+
+    public void UnspeedGrapple()
+    {
+        _currentGrappleSpeed = grappleSpeed;
     }
 
 }
